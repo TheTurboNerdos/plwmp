824a53b549176dc4fed63e597dcf77a1ad1e5e08
Black-Tomcat <tomcat.frantz@gmail.com>
2018-10-17 17:08:28 +1100

    Added template for DashGrid.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
+import DashGrid from './containers/DashGrid';
 
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                Dummy Info
-            </div>
+            <DashGrid/>
         )
     }
 }
---
+++ src/containers/DashGrid.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+/**
+ *
+ * The DashGrid.
+ * This class is here to be the grid layout for the project.
+ *
+ */
+export default class DashGrid extends Component {
+    render() {
+        return (
+            <div>
+                DashGrid Placeholder.
+            </div>
+        )
+    }
+}
